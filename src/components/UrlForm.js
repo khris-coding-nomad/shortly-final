47d696c4a66f203e13df7e5fe365846d62dbf51b
Khristinasheer <khristinasar@gmail.com>
2021-02-03 22:47:37 -0500

    add form updates

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -4,21 +4,21 @@ const UrlForm = () => (
   <div className="mt-10 sm:mt-12">
     <form className="sm:mx-auto lg:mx-0">
       <div className="sm:flex">
-        <div classname="flex-1 min-w-0">
+        <div classname="min-w-0 flex-1">
           <label htmlFor="url" className="sr-only">
             URL Input
           </label>
           <input
             type="text"
             name="url"
-            placeholder="Place your link..."
-            className="block w-full px-4 py-3 rounded-md border-0 text-base tetx-gray-900 placeholder-gray-500"
+            placeholder="Place your link here..."
+            className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
           />
         </div>
         <div className="mt-3 sm:mt-0 sm:ml-3">
           <button
             type="submit"
-            className="block w-full py-3 px-4 h-12 rounded-md bg-gradient-to-r from-indigo-200 to-indigo-400 text-white font-bold hover:from-indigo-600 hover:to-indigo-700"
+            className="block w-full py-3 px-4 h-12 rounded-md shadow bg-gradient-to-r from-indigo-200 to-indigo-400 text-white font-bold hover:from-indigo-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
           >
             Shorten it!
           </button>
